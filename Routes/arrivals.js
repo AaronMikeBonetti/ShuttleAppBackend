d289114ce4060ef26102ab6b0115b0ffe977ccde
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-07-09 17:29:57 -0400

    testing api fetch

--- Routes/arrivals.js
+++ Routes/arrivals.js
@@ -5,8 +5,8 @@ let Axios = require('axios')
 
 let fetchFlights = () =>{
     const baseUrl = 'https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/KMCO/arr/'
-    const appId = '91671c55'
-    const appKey = '75376fdd738335ffa0f2b61ecac5dd03&utc'
+    const appId = '85270bc1'
+    const appKey = '3526508e3dbaa843f4d918e5e34b5970'
     const flights = []
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.json())
@@ -20,8 +20,7 @@ let fetchFlights = () =>{
 }
 
 router.route('/').get((req,res)=>{
-    const flightsArray = []
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch('https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/KMCO/arr/2019/7/9/17?appId=85270bc1&appKey=3526508e3dbaa843f4d918e5e34b5970&utc=true&numHours=6&carrier=f9')
     .then((response) => response.json())
    
     .then((data)=>{
